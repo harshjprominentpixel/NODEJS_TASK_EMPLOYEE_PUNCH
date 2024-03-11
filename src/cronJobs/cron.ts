0dfeb64ea92f7058676d36ac8a30cf93c2141826
Harsh Jolapara <harsh.jolapara@prominentpixel.com>
2024-03-11 15:09:58 +0530

    every 3 hour move files

--- src/cronJobs/cron.ts
+++ src/cronJobs/cron.ts
@@ -1,7 +1,39 @@
 const cron = require("node-cron");
+const fs = require("fs");
+const path = require("path");
 
 export const copyAllFilesCron = () => {
-  cron.schedule("* * * * *", () => {
-    console.log("running a task every minute");
+  cron.schedule("0 */3 * * *", () => {
+    const sourceFolder = "./generatedPDFs";
+    const destinationFolder = "./movedGeneratedFilePDFs";
+
+    if (!fs.existsSync(sourceFolder)) {
+      console.error("Source folder not found!");
+      return;
+    }
+
+    if (!fs.existsSync(destinationFolder)) {
+      fs.mkdirSync(destinationFolder);
+    }
+
+    fs.readdir(sourceFolder, (err: Error, files: File[]) => {
+      if (err) {
+        console.error("Error reading source folder:", err);
+        return;
+      }
+
+      files.forEach((file) => {
+        const sourceFilePath = path.join(sourceFolder, file);
+        const destinationFilePath = path.join(destinationFolder, file);
+
+        fs.rename(sourceFilePath, destinationFilePath, (err: Error) => {
+          if (err) {
+            console.error("Error moving file:", err);
+          } else {
+            console.log(`Moved file: ${file}`);
+          }
+        });
+      });
+    });
   });
 };
